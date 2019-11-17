2a2cd5c2cca590127530087febadffe77c2e793b
tieda <lamplighter.1992@gmail.com>
2019-11-17 15:25:53 +0700

    simple list

--- src/Inheritance/Geometry/Main.java
+++ src/Inheritance/Geometry/Main.java
@@ -6,40 +6,6 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
 
-        Shape shape = new Shape();
-        System.out.println(shape);
-
-        shape = new Shape("red", false);
-        System.out.println(shape);
-
-        Circle circle = new Circle();
-        System.out.println(circle);
-
-        circle = new Circle(3.5);
-        System.out.println(circle);
-
-        circle = new Circle("indigo", false, 3.5);
-        System.out.println(circle);
-
-        Rectangle rectangle = new Rectangle();
-        System.out.println(rectangle);
-
-        rectangle = new Rectangle(2.3, 5.8);
-        System.out.println(rectangle);
-
-        rectangle = new Rectangle("orange", true, 2.3, 5.8);
-        System.out.println(rectangle);
-
-        Square square = new Square();
-        System.out.println(square);
-
-        square = new Square(2.3);
-        System.out.println(square);
-
-        square = new Square("yellow", true, 5.8);
-        System.out.println(square);
-
-
         ComparableCircle[] circles = new ComparableCircle[5];
         circles[0] = new ComparableCircle(3.6);
         circles[1] = new ComparableCircle();
